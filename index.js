1393b71b213c8a9064d4ffaec05621ec9d41c958
HagglundJonatan <hagglund.jonatan@protonmail.com>
2025-02-25 23:56:37 +0100

    fix fonts

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 const port = 3002;
 
-console.log("hej")
 app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.get('/', function(req, res) {
