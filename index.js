49c1a0e331071c316fc4db65f7fd4b347a6dd1f6
HagglundJonatan <hagglund.jonatan@protonmail.com>
2025-03-02 10:35:29 +0100

    fix package.json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = 3002;
 
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, '/public'))); 
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
